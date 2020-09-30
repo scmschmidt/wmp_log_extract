36d14a8f381361024160e033139df980cb7f512e
Sören Schmidt <soeren.schmidt@suse.com>
2020-09-30 18:06:18 +0200

    first release

--- README.md
+++ README.md
@@ -1,1 +1,131 @@
 # wmp_log_extract
+
+This tool will parse given system log files or all messages, even archived ones,
+in `/var/log/*` for entries of `wmp_memory_current` and present the data as 
+human-readable table or as CSV file.
+
+It is possible to limit the data to certain timestamps, cgroups or data and convert
+the output to different units (KiB, MiB or GiB).  
+
+The human readable output also lists the maximum and minimum values
+
+CSV output can be used as import to other tools for further analysis or drawing graphs.
+
+`wmp_memory_current` is part of the sapwmp package. For details read the chapter about
+Workload Memory Protection in the official Guide of SLES for SAP Applications 15.
+
+[SLES for SAP Applications SLES 15 SP2 - WMP](https://documentation.suse.com/sles-sap/15-SP2/html/SLES-SAP-guide/cha-tune.html#sec-memory-protection)
+
+## Usage
+```
+wmp_log_extract.py [-h] [--timestamp PATTERN]
+                          [--unit {B,kB,kiB,MB,MiB,GB,GiB}]
+                          [--cgroup CGROUP,...] [--param PARAM,...] [--sort]
+                          [--csv] [--delim DELIM]
+                          [LOGFILE [LOGFILE ...]]
+
+Extracts WMP cgroup memory data from system log and prints it as CSV or as a
+human-readable table.
+
+positional arguments:
+  LOGFILE               (xz compressed) system log file (default is /var/log/messages*)
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --timestamp PATTERN   regular expression used to filter timestamps
+  --unit {B,kB,kiB,MB,MiB,GB,GiB}
+                        unit for memory values
+  --cgroup CGROUP,...   extract only these cgroups
+  --param PARAM,...     extract only these controller parameters
+  --sort                sort timestamps
+  --csv                 print data as CSV with delimiter character (default is
+                        comma)
+  --delim DELIM         delimiter character for CSV output
+```
+
+## Examples
+
+### Getting an overview of all collected data in the system log
+
+```
+ # ./wmp_log_extract.py
+
+           timestamp             SAP.slice    SAP.slice    user.slice   user.slice   init.scope   init.scope   data-protector.socket data-protector.socket csync2.socket csync2.socket  rpcbind.socket rpcbind.socket system.slice  system.slice 
+                                 memory.low memory.current memory.low memory.current memory.low memory.current      memory.low          memory.current      memory.low   memory.current   memory.low   memory.current  memory.low  memory.current
+================================ ========== ============== ========== ============== ========== ============== ===================== ===================== ============= ============== ============== ============== ============ ==============
+2020-09-18T08:10:59.506261+00:00          0              0          0       92217344          0       10584064                     0                  4096             0           4096              0          12288            0     1198563328
+2020-09-18T08:21:05.958765+00:00          0              0          0      655872000          0       10125312                     0                  4096             0           4096              0          12288            0     1246830592
+2020-09-18T08:30:58.541972+00:00          0              0          0      689807360          0       10354688                     0                  4096             0           4096              0          12288            0     1249996800
+2020-09-18T08:42:50.716478+00:00          0    53765943296          0      788279296          0       12677120                     0                  4096             0           4096              0          12288            0     1973104640
+...
+2020-09-28T23:31:47.894334+00:00          0    66897539072          0     1268387840          0       57430016                     0                  4096             0           4096              0          12288            0     2255523840
+2020-09-28T23:40:26.761930+00:00          0    66980184064          0     1280581632          0       57647104                     0                  4096             0           4096              0          12288            0     2271358976
+2020-09-28T23:50:36.559319+00:00          0    66981888000          0     1279131648          0       57384960                     0                  4096             0           4096              0          12288            0     2275016704
+
+                                 SAP.slice    SAP.slice    user.slice   user.slice   init.scope   init.scope   data-protector.socket data-protector.socket csync2.socket csync2.socket  rpcbind.socket rpcbind.socket system.slice  system.slice 
+                                 memory.low memory.current memory.low memory.current memory.low memory.current      memory.low          memory.current      memory.low   memory.current   memory.low   memory.current  memory.low  memory.current
+================================ ========== ============== ========== ============== ========== ============== ===================== ===================== ============= ============== ============== ============== ============ ==============
+                         minimum          0              0          0       92217344          0       10125312                     0                  4096             0           4096              0          12288            0     1198563328
+                         maximum          0   109700108288          0     3650109440          0      226021376                     0                155648             0           4096              0          12288            0    26687668224
+```
+
+You can see for each timestamp the values of `memory.low` and `memory.current` for each cgroup at the first level. Below also the maximum and minimum value is displayed.
+The unit is byte.
+
+### Display only the data for the `SAP.slice` on a specific day with a more convinient unit
+
+```
+ # ./wmp_log_extract.py --unit GiB --timestamp '^2020-09-28' --cgroup SAP.slice 
+
+           timestamp             SAP.slice    SAP.slice   
+                                 memory.low memory.current
+================================ ========== ==============
+2020-09-28T00:00:30.632187+00:00        0.0          101.5
+2020-09-28T00:10:41.036439+00:00        0.0          101.5
+2020-09-28T00:22:21.723392+00:00        0.0          101.5
+...
+2020-09-28T23:40:26.761930+00:00        0.0           62.4
+2020-09-28T23:50:36.559319+00:00        0.0           62.4
+
+                                 SAP.slice    SAP.slice   
+                                 memory.low memory.current
+================================ ========== ==============
+                         minimum        0.0            0.0
+                         maximum        0.0          102.2
+```
+
+### Display only the data for `memory.current` and `memory.swap.current` of all cgroups on a specific month with a more convinient unit 
+
+```
+ # ./wmp_log_extract.py  --unit GiB --timestamp '^2020-09' --param memory.current,memory.swap.current
+
+           timestamp               SAP.slice         SAP.slice        user.slice       user.slice        init.scope       init.scope       system.slice     system.slice    
+                                 memory.current memory.swap.current memory.current memory.swap.current memory.current memory.swap.current memory.current memory.swap.current
+================================ ============== =================== ============== =================== ============== =================== ============== ===================
+2020-09-01T00:00:01.477420+02:00            3.3                 0.0            1.9                 0.0            0.0                 0.0            0.7                 0.0
+2020-09-01T00:02:57.964914+02:00            3.3                 0.0            1.9                 0.0            0.0                 0.0            0.7                 0.0
+2020-09-01T00:03:08.768000+02:00            3.3                 0.0            1.9                 0.0            0.0                 0.0            0.7                 0.0
+...
+2020-09-30T17:51:25.047972+02:00            3.3                 0.0            2.0                 0.0            0.0                 0.0            0.7                 0.0
+2020-09-30T17:52:50.021523+02:00            3.3                 0.0            2.0                 0.0            0.0                 0.0            0.7                 0.0
+2020-09-30T17:54:25.047338+02:00            3.3                 0.0            2.0                 0.0            0.0                 0.0            0.7                 0.0
+
+                                   SAP.slice         SAP.slice        user.slice       user.slice        init.scope       init.scope       system.slice     system.slice    
+                                 memory.current memory.swap.current memory.current memory.swap.current memory.current memory.swap.current memory.current memory.swap.current
+================================ ============== =================== ============== =================== ============== =================== ============== ===================
+                         minimum            3.3                 0.0            1.9                 0.0            0.0                 0.0            0.7                 0.0
+                         maximum            3.3                 0.0            2.2                 0.0            0.0                 0.0            0.7                 0.0
+```
+
+### Create a CSV file with all data and enforce sorted timestamps
+
+```
+# ./wmp_log_extract.py --csv --sort > wmp_data.csv
+
+# cat wmp_data.csv
+timestamp,SAP.slice/memory.low,SAP.slice/memory.current,SAP.slice/memory.swap.current,user.slice/memory.low,user.slice/memory.current,user.slice/memory.swap.current,init.scope/memory.low,init.scope/memory.current,init.scope/memory.swap.current,system.slice/memory.low,system.slice/memory.current,system.slice/memory.swap.current
+2020-09-24T00:00:01.477420+02:00,16106127360,3552940032,0,0,2083557376,0,0,7647232,0,0,731123712,0
+2020-09-24T00:02:57.964914+02:00,16106127360,3552976896,0,0,2080681984,0,0,7421952,0,0,706076672,0
+2020-09-24T00:03:08.768000+02:00,16106127360,3552919552,0,0,2080833536,0,0,7467008,0,0,705712128,0
+...
+```
---
+++ wmp_log_extract.py
@@ -0,0 +1,287 @@
+#!/usr/bin/env python3
+# -*-
+
+'''
+------------------------------------------------------------------------------
+Copyright (c) 2019 SUSE LLC
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of version 3 of the GNU General Public License as published by the
+Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, contact SUSE Linux GmbH.
+
+------------------------------------------------------------------------------
+Author: Sören Schmidt <soeren.schmidt@suse.com>
+
+This tool checks if WMP is set up correctly. 
+
+Exit codes:        0   All checks ok. WMP has been set up correctly.
+                   1   TODO
+
+Changelog:
+
+30.09.2020  v1.0  First release.
+'''
+
+import argparse
+import collections
+import glob
+import lzma
+import os
+import re
+import signal
+import sys
+    
+__version__ = '1.0'
+
+
+def signal_handler(signal, frame):
+    """ Terminate with exit code 1. """
+    sys.exit(1)
+
+
+def print_err(text):
+    """ Prints text on stderr. """
+    sys.stderr.write("%s\n" % text)
+
+
+def exit_on_error(text, exitcode=1):
+    """ Prints text on stderr and terminates with exitcode. """
+    sys.stderr.write(text)
+    sys.exit(exitcode)
+
+
+def parse_arguments():
+    """ Evaluate commandline arguments. """
+
+    program_description = 'Extracts WMP cgroup memory data from system log and prints it as CSV or as a human-readable table.'
+
+    parser = argparse.ArgumentParser(description=program_description)
+    parser.add_argument('--timestamp', action='store', dest='timestamp_pattern', metavar='PATTERN', help='regular expression used to filter timestamps', default=False)
+    parser.add_argument('--unit', action='store', dest='unit', choices=['B', 'kB', 'kiB', 'MB', 'MiB', 'GB', 'GiB'],help='unit for memory values', default='B')
+    parser.add_argument('--cgroup', action='store', dest='cgroups_wanted', metavar='CGROUP,...', help='extract only these cgroups', default=None)
+    parser.add_argument('--param', action='store', dest='params_wanted', metavar='PARAM,...', help='extract only these controller parameters', default=None)
+    parser.add_argument('--sort', action='store_true', dest='sorting', help='sort timestamps', default=False)
+    parser.add_argument('--csv', action='store_true', dest='csv', help='print data as CSV with delimiter character (default is comma)', default=False)
+    parser.add_argument('--delim', action='store', dest='delim', help='delimiter character for CSV output', default=',')
+    parser.add_argument('logfile', metavar ='LOGFILE', type = str, nargs ='*', help ='(xz compressed) system log file (default is /var/log/messages*)') 
+    arguments = parser.parse_args()
+    
+    # If no logfiles have been provided we take a sorted list of all messages in /var/log/.
+    if len(arguments.logfile) == 0:
+        arguments.logfile = sorted(glob.glob('/var/log/messages-*'))
+        if os.path.exists('/var/log/messages'):
+            arguments.logfile.append('/var/log/messages')
+
+    # Convert cgroups_wanted and params_wanted into lists, if they have been used.
+    if arguments.cgroups_wanted:
+        arguments.cgroups_wanted = arguments.cgroups_wanted.split(',')
+    if arguments.params_wanted:
+        arguments.params_wanted = arguments.params_wanted.split(',')
+
+    # Transform unit in factor.
+    arguments.factor = {'B': 1, 'kB': 1024, 'kiB': 1024, 'MB':1024*1024, 'MiB':1024*1024, 'GB':1024*1024*1024, 'GiB':1024*1024*1024}[arguments.unit]
+
+    # Convert from/to timestamps into datetime objects.
+    try:
+        if arguments.timestamp_pattern:
+            arguments.timestamp_pattern = re.compile(arguments.timestamp_pattern)
+    except Exception as err:
+        exit_on_error('Timestamp pattern is invalid: %s\n' % err) 
+
+    return arguments
+
+
+def read_logs(filenames, cgroups_wanted, params_wanted, timestamp_pattern, factor=1):
+    """ Reads logfiles and returns an ordered dictionary. """
+
+    cgroup_data = collections.OrderedDict()
+    column_width = {}
+    found_cgroup_params = []
+    filter = re.compile(' wmp_memory_current: ')
+
+    for filename in filenames:
+
+        # Open the (compressed) file.
+        try:
+            if filename.endswith('.xz'):
+                fh = lzma.open(filename, mode='rt')
+            else:
+                fh = open(filename, mode='r')     
+        except Exception as err:
+            print_err('Could not read %s: %s' % (filename, err))
+            continue
+            
+        # Read in each line that matches the filter.
+        for line in fh.readlines():
+            if not filter.search(line):
+                continue
+            if 'Exiting.' in line:
+                continue
+
+            # Extract the data and build up the dictionary.
+            try:
+                timestamp, _, _, data = line.strip().split(maxsplit=3)
+                if timestamp_pattern:  # except entries that does not match the timestamp pattern
+                    if not timestamp_pattern.match(timestamp):
+                        continue
+                        print('%s matches!' % timestamp)
+                for section in data.split(','):
+                    cgroup, dataset = section.split(':')
+                    cgroup = cgroup.strip()
+                    if cgroups_wanted and cgroup not in cgroups_wanted:
+                        continue
+                    for pair in  dataset.split():
+                        param, value = pair.split('=')
+                        if params_wanted and param not in params_wanted:
+                            continue
+                        if value == '-':
+                            continue
+                        if factor != 1:
+                            value = '{:.1f}'.format(int(value)/factor)
+                        key = '{}/{}'.format(cgroup, param)
+                        cgroup_data.setdefault(timestamp, {})[key] = value
+                        if key not in found_cgroup_params:
+                            found_cgroup_params.append(key)
+                        column_width['timestamp'] = max(len(timestamp), column_width.get('timestamp', 9))
+                        column_width[key] = max(len(cgroup), len(param), len(value), column_width.get(key, 0))
+                    
+            except Exception as err:
+                print_err('Error \"%s\" parsing line: %s' % (err, line.strip()))
+                continue
+                
+        # Close file.
+        fh.close()
+
+    return cgroup_data, found_cgroup_params, column_width
+
+
+def print_csv(cgroup_data, found_cgroup_params, separator = ',', sorting=False):
+    """ Prints log data as csv. """
+
+    if sorting:
+        timestamps = sorted(cgroup_data.keys())
+    else:
+        timestamps = list(cgroup_data.keys())
+
+    # Print header.
+    header_string = 'timestamp%s%s' % (separator, separator.join(found_cgroup_params))
+    print(header_string)
+
+    # Print data.
+    for timestamp in timestamps:
+        data = cgroup_data[timestamp]
+        line = []
+        for name in found_cgroup_params: # we don't go through keys of column_with because we need a reproducable order of the columns
+            if name in data:
+                value = data[name]
+            else:
+                value = '-'
+            line.append(value)    
+        print("%s%s%s" % (timestamp, separator, separator.join(line)))
+
+
+def print_humanreadable(cgroup_data, found_cgroup_params, column_width, sorting=False):
+    """ Prints log data as a nice human-readable table. """
+
+    max_val, min_val = {}, {}
+
+    if sorting:
+        timestamps = sorted(cgroup_data.keys())
+    else:
+        timestamps = list(cgroup_data.keys())
+
+    # Prepare headers.
+    field_format = '{text:^{width}s}'
+    header_string_1a = [field_format.format(text='timestamp', width=column_width['timestamp'])]
+    header_string_1b = [field_format.format(text=' '*column_width['timestamp'], width=column_width['timestamp'])]
+    header_string_2 = [field_format.format(text=' '*column_width['timestamp'], width=column_width['timestamp'])]
+    ruler = [field_format.format(text='='*column_width['timestamp'], width=column_width['timestamp'])]
+    for col in found_cgroup_params:  # we don't go through keys of column_with because we need a reproducable order of the columns
+        width = column_width[col]
+        cgroup, param = col.split('/')
+        header_string_1a.append(field_format.format(text=cgroup, width=width))
+        header_string_1b.append(field_format.format(text=cgroup, width=width)) 
+        header_string_2.append(field_format.format(text=param, width=width))
+        ruler.append(field_format.format(text='='*width, width=width))
+
+    # Print data and calculate maximum and minimum.
+    print('\n%s\n%s\n%s' % (' '.join(header_string_1a), ' '.join(header_string_2), ' '.join(ruler)))
+    field_format = '{text:>{width}s}'
+    pattern_int = re.compile('[0-9]+')
+    pattern_float  = re.compile('[0-9]+\.[0-9]+')
+    for timestamp in timestamps:
+        data = cgroup_data[timestamp]
+        line = []
+        for name in found_cgroup_params: # we don't go through keys of column_with because we need a reproducable order of the columns
+            if name in data:
+                value = data[name]
+                if pattern_int.fullmatch(value):  # we have an integer
+                    value = int(value)
+                elif pattern_float.fullmatch(value):  # we have a float
+                    value = float(value)
+                try:
+                    max_val[name] = max(max_val.setdefault(name, value) , value)
+                    min_val[name] = min(min_val.setdefault(name, value) , value)
+                except:
+                    pass           
+            else:
+                value = '-'
+            line.append(field_format.format(text=str(value), width=column_width[name]))
+        print("%s %s" % (timestamp, ' '.join(line)))
+
+    # Print naximum and minimum.
+    print('\n%s\n%s\n%s' % (' '.join(header_string_1b), ' '.join(header_string_2), ' '.join(ruler)))
+    line_max, line_min = [], []
+    for name in found_cgroup_params:
+        if name in max_val:
+            value_max = max_val[name]
+        else:
+            value_max = '-'
+        if name in min_val:
+            value_min = min_val[name]
+        else:
+            value_min = '-'
+        line_max.append(field_format.format(text=str(value_max), width=column_width[name]))
+        line_min.append(field_format.format(text=str(value_min), width=column_width[name]))    
+    print('%s %s' % (field_format.format(text='minimum', width=column_width['timestamp']), ' '.join(line_min)))
+    print('%s %s' % (field_format.format(text='maximum', width=column_width['timestamp']), ' '.join(line_max)))
+
+
+def main():
+
+    # Avoid "IOError: [Errno 32] Broken pipe" when pipe ootput to other programs.
+    # http://coding.derkeiler.com/Archive/Python/comp.lang.python/2004-06/3823.html
+    signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+
+    # Terminate nicely at ^C.
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # Get arguments.
+    arguments = parse_arguments()
+
+    # Read log files.
+    cgroup_data, found_cgroup_params, column_width = read_logs(arguments.logfile, arguments.cgroups_wanted, arguments.params_wanted, arguments.timestamp_pattern, arguments.factor)
+
+    # Exit if we don't have any data collected.
+    if len(cgroup_data) == 0:
+        exit_on_error('No WMP data found! Exiting.\n', 1)
+
+    # Print data as CSV or human-readable .
+    if arguments.csv:
+        print_csv(cgroup_data, found_cgroup_params, arguments.delim, arguments.sorting)
+    else:
+        print_humanreadable(cgroup_data, found_cgroup_params, column_width, arguments.sorting)
+
+    # Bye.
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
